0fc6a0bbfa7a8ae587fae1bbad60ccaed606a17f
Zihao Wang <416990273@qq.com>
2017-03-16 15:51:10 +0800

    navigaton接口添加

--- ios/objNative/JSNavigation.h
+++ ios/objNative/JSNavigation.h
@@ -8,6 +8,8 @@
 
 
 #import <React/RCTBridgeModule.h>
-@interface JSNavigation : NSObject<RCTBridgeModule>
+#import <React/RCTEventEmitter.h>
+#import "SuperMap/Navigation.h"
+@interface JSNavigation : RCTEventEmitter<RCTBridgeModule,NaviListener>
 
 @end
